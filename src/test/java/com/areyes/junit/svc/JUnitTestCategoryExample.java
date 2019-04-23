e5f31ea0e8707eb62cd7c1ca4faf6dc26e15c464
serghalak <serghalak@gmail.com>
2019-04-23 12:09:27 +0300

    add test group

--- src/test/java/com/areyes/junit/svc/JUnitTestCategoryExample.java
+++ src/test/java/com/areyes/junit/svc/JUnitTestCategoryExample.java
@@ -1,5 +1,63 @@
 package com.areyes.junit.svc;
 
+import static org.hamcrest.CoreMatchers.isA;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.areyes.junit.cat.intf.FunctionalGroupTests1;
+import com.areyes.junit.cat.intf.FunctionalGroupTests2;
+import com.areyes.junit.cat.intf.IntegrationTests;
+import com.areyes.junit.cat.intf.SanityTests;
+
 public class JUnitTestCategoryExample {
 
+	@Test
+	@Category(FunctionalGroupTests1.class)
+	public void testFunctionalTests1Test1() {
+		int numberInLoop=0;
+		for(int i=0;i<1000;i++) {
+			numberInLoop++;
+		}
+		System.out.println("functionalgroupTests1: testFunctionalTest1Test1");
+		Assert.assertThat(numberInLoop, isA(Integer.class));
+	}
+	
+	@Test
+	@Category(FunctionalGroupTests1.class)
+	public void testFunctionalTests1Test2() {
+		int numberInLoop=0;
+		for(int i=1000;i<4000;i++) {
+			numberInLoop++;
+		}
+		System.out.println("functionalgroupTests1: testFunctionalTest1Test2");
+		Assert.assertThat(numberInLoop, isA(Integer.class));
+	}
+	
+	@Test @Category(FunctionalGroupTests2.class)
+	public void testFunctionalTests2Test1() {
+		// You’re test case here: Below is just an example.
+		int numberInLoop = 0;
+		do{
+			numberInLoop++;
+		}while(numberInLoop != 1000);
+		System.out.println("FunctionalGroupTests2: testFunctionalTests2Test1");
+		Assert.assertThat(numberInLoop,isA(Integer.class));
+	}
+	
+	@Test @Category(FunctionalGroupTests2.class)
+	public void testFunctionalTests2Test2() {
+		System.out.println("FunctionalGroupTests2: testFunctionalTests2Test2");
+	}
+	
+	@Test @Category({IntegrationTests.class,FunctionalGroupTests1.class})
+	public void testIntegrationTestsTest1() {
+		System.out.println("IntegrationTests: testIntegrationTestsTest1");
+	}
+	
+	@Test @Category(SanityTests.class)
+	public void testSanityTestsTest1() {
+		System.out.println("SanityTests: testSanityTestsTest1");
+	}
 }
